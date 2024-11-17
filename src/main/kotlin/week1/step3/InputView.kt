314d4872d1ba4d6ce5ea1be023450609549e9dda
김창민 <changmin@gimchangmin-ui-MacBookPro.local>
2024-11-17 19:13:38 +0900

    inputView 피드백 반영 - view 개체의 역할과 책임 고려, 불필요한 상태 관리 제거, null 입력에 대한 체크 및 가독성 고려 코드 개선

--- src/main/kotlin/week1/step3/InputView.kt
+++ src/main/kotlin/week1/step3/InputView.kt
@@ -1,21 +1,21 @@
 package week1.step3
 
 class InputView {
-    var numberOfCars: Int = 0
-    var numberOfAttempts: Int = 0
-
-    private fun inputNumberOfCars() {
+    private fun inputNumberOfCars(): Int {
         println("자동차 대수는 몇 대인가요? ")
-        numberOfCars = readLine()!!.toInt()
+        val inputNumberOfCars: Int? = readlnOrNull()?.toIntOrNull()
+        requireNotNull(inputNumberOfCars) { "자동차 대수가 올바르게 입력되지 않았습니다" }
+        return inputNumberOfCars.toInt()
     }
 
-    private fun inputAttemptCount() {
+    private fun inputAttemptCount(): Int {
         println("시도할 횟수는 몇 회인가요? ")
-        numberOfAttempts = readLine()!!.toInt()
+        val inputAttemptCount: Int? = readlnOrNull()?.toIntOrNull()
+        requireNotNull(inputAttemptCount) { "시도할 횟수가 올바르게 입력되지 않았습니다" }
+        return inputAttemptCount.toInt()
     }
 
-    fun view() {
-        inputNumberOfCars()
-        inputAttemptCount()
+    fun view(): Pair<Int, Int> {
+        return Pair(inputNumberOfCars(), inputAttemptCount())
     }
 }
