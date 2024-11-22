514f2acf89d4c98bb5faa97c2ad8cff0e127315d
Pablo730 <kcm1619@naver.com>
2024-11-23 00:05:25 +0900

    기능 목록 수정 및 경주에서 자동차 전진을 시도하는 횟수가 1 미만이면 예외 발생에 대한 기능 구현 및 테스트

--- src/main/kotlin/racingcar/Racing.kt
+++ src/main/kotlin/racingcar/Racing.kt
@@ -1,11 +1,13 @@
 package racingcar
 
-class Racing(cars: List<Car>){
+class Racing(val cars: List<Car>, private val forwardAttemptCount: Int){
     init {
-        require(cars.isNotEmpty()) { INVALID_RACING_CAR_LIST_MESSAGE }
+        require(cars.isNotEmpty()) { INVALID_RACING_CARS_MESSAGE }
+        require(forwardAttemptCount > 0) { INVALID_FORWARD_ATTEMPT_COUNT_MESSAGE }
     }
 
     companion object {
-        const val INVALID_RACING_CAR_LIST_MESSAGE: String = "자동차 경주에는 최소 1대 이상 참가하는 자동차가 있어야합니다"
+        const val INVALID_RACING_CARS_MESSAGE: String = "자동차 경주에는 최소 1대 이상 참가하는 자동차가 있어야합니다"
+        const val INVALID_FORWARD_ATTEMPT_COUNT_MESSAGE: String = "자동차 경주에는 최소 1회 이상 전진을 시도 해야합니다"
     }
 }
--- src/test/kotlin/racingcar/RacingTest.kt
+++ src/test/kotlin/racingcar/RacingTest.kt
@@ -1,14 +1,29 @@
 package racingcar
 
 import io.kotest.assertions.throwables.shouldThrowWithMessage
+import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
-import racingcar.Racing.Companion.INVALID_RACING_CAR_LIST_MESSAGE
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
+import racingcar.Racing.Companion.INVALID_FORWARD_ATTEMPT_COUNT_MESSAGE
+import racingcar.Racing.Companion.INVALID_RACING_CARS_MESSAGE
 import java.lang.IllegalArgumentException
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class RacingTest {
     @Test
     fun `자동차 경주에 참여하는 자동차가 존재하지 않으면 예외가 발생한다`() {
-        shouldThrowWithMessage<IllegalArgumentException>(message = INVALID_RACING_CAR_LIST_MESSAGE) {
-            Racing(cars = listOf()) }
+        shouldThrowWithMessage<IllegalArgumentException>(message = INVALID_RACING_CARS_MESSAGE) {
+            Racing(cars = listOf(), forwardAttemptCount = 1)
+        }
+    }
+
+    @ValueSource(ints = [-1, 0])
+    @ParameterizedTest
+    fun `자동차 경주의 전진 시도 횟수가 1 미만이면 예외가 발생한다`(forwardAttemptCount: Int) {
+        shouldThrowWithMessage<IllegalArgumentException>(message = INVALID_FORWARD_ATTEMPT_COUNT_MESSAGE) {
+            Racing(cars = listOf(Car("Pablo")), forwardAttemptCount = forwardAttemptCount)
+        }
     }
 }
