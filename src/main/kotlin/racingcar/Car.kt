57821912c36edb339447d2e6dc13c9cfbaffa541
Pablo730 <kcm1619@naver.com>
2024-11-22 16:28:20 +0900

    자동차의 이름이 빈 값이거나, 5글자를 넘으면 예외 발생 기능 구현 및 테스트

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,8 +1,14 @@
 package racingcar
 
 class Car(val name: String, val position: Int = DEFAULT_CAR_POSITION) {
+    init {
+        require(name.length in CAR_NAME_LENGTH_MIN .. CAR_NAME_LENGTH_MAX) { INVALID_CAR_NAME_MESSAGE }
+    }
 
     companion object {
         private const val DEFAULT_CAR_POSITION: Int = 0
+        private const val CAR_NAME_LENGTH_MIN: Int = 1
+        private const val CAR_NAME_LENGTH_MAX: Int = 5
+        const val INVALID_CAR_NAME_MESSAGE: String = "자동차 이름이 빈값이나 5글자를 넘을 수 없습니다"
     }
 }
--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,7 +1,11 @@
 package racingcar
 
+import io.kotest.assertions.throwables.shouldThrow
+import io.kotest.assertions.throwables.shouldThrowWithMessage
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import java.lang.IllegalArgumentException
+import racingcar.Car.Companion.INVALID_CAR_NAME_MESSAGE
 
 class CarTest {
     @Test
@@ -15,5 +19,11 @@ class CarTest {
     fun `자동차의 초기 위치는 0이다`() {
         val car = Car(name = "Pablo")
         car.position shouldBe 0
+    }
+
+    @Test
+    fun `자동차의 이름이 빈 값이거나 5글자를 넘으면 예외가 발생한다`() {
+        shouldThrowWithMessage<IllegalArgumentException>(message = INVALID_CAR_NAME_MESSAGE) { Car("") }
+        shouldThrowWithMessage<IllegalArgumentException>(message = INVALID_CAR_NAME_MESSAGE) { Car("NEXT_STEP") }
     }
 }
