8126504300d4481c172fdec7470745964f7ac029
Pablo730 <kcm1619@naver.com>
2024-11-22 16:36:23 +0900

    자동차 전진의 기준 값은 4이상이면 전진한다에 대한 기능 구현 및 테스트

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,8 +1,14 @@
 package racingcar
 
-class Car(val name: String, val position: Int = DEFAULT_CAR_POSITION) {
+class Car(val name: String, var position: Int = DEFAULT_CAR_POSITION) {
     init {
         require(name.length in CAR_NAME_LENGTH_MIN .. CAR_NAME_LENGTH_MAX) { INVALID_CAR_NAME_MESSAGE }
+    }
+
+    fun forward(condition: Int) {
+        if (condition >= CAR_FORWARD_STANDARD_CONDITION) {
+            position++
+        }
     }
 
     companion object {
@@ -10,5 +16,6 @@ class Car(val name: String, val position: Int = DEFAULT_CAR_POSITION) {
         private const val CAR_NAME_LENGTH_MIN: Int = 1
         private const val CAR_NAME_LENGTH_MAX: Int = 5
         const val INVALID_CAR_NAME_MESSAGE: String = "자동차 이름이 빈값이나 5글자를 넘을 수 없습니다"
+        const val CAR_FORWARD_STANDARD_CONDITION: Int = 4
     }
 }
--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -1,12 +1,15 @@
 package racingcar
 
-import io.kotest.assertions.throwables.shouldThrow
 import io.kotest.assertions.throwables.shouldThrowWithMessage
 import io.kotest.matchers.shouldBe
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.TestInstance
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.ValueSource
 import java.lang.IllegalArgumentException
 import racingcar.Car.Companion.INVALID_CAR_NAME_MESSAGE
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 class CarTest {
     @Test
     fun `자동차는 이름과 위치를 가진다`() {
@@ -25,5 +28,13 @@ class CarTest {
     fun `자동차의 이름이 빈 값이거나 5글자를 넘으면 예외가 발생한다`() {
         shouldThrowWithMessage<IllegalArgumentException>(message = INVALID_CAR_NAME_MESSAGE) { Car("") }
         shouldThrowWithMessage<IllegalArgumentException>(message = INVALID_CAR_NAME_MESSAGE) { Car("NEXT_STEP") }
+    }
+
+    @ValueSource(ints = [4, 5, 6, 7, 8, 9])
+    @ParameterizedTest
+    fun `자동차에 주어진 무작위 값이 4 이상이면 전진한다`(condition: Int) {
+        val car = Car(name = "Pablo", 2)
+        car.forward(condition = condition)
+        car.position shouldBe 3
     }
 }
