067e3b09f27d258ad20e040675e8683ec0a32d0d
Pablo730 <kcm1619@naver.com>
2024-11-22 16:14:09 +0900

    자동차의 초기 위치는 0이다 기능 구현 및 테스트

--- src/main/kotlin/racingcar/Car.kt
+++ src/main/kotlin/racingcar/Car.kt
@@ -1,4 +1,8 @@
 package racingcar
 
-class Car(val name:String, val position:Int) {
+class Car(val name: String, val position: Int = DEFAULT_CAR_POSITION) {
+
+    companion object {
+        private const val DEFAULT_CAR_POSITION: Int = 0
+    }
 }
--- src/test/kotlin/racingcar/CarTest.kt
+++ src/test/kotlin/racingcar/CarTest.kt
@@ -10,4 +10,10 @@ class CarTest {
         car.name shouldBe "Pablo"
         car.position shouldBe 3
     }
+
+    @Test
+    fun `자동차의 초기 위치는 0이다`() {
+        val car = Car(name = "Pablo")
+        car.position shouldBe 0
+    }
 }
